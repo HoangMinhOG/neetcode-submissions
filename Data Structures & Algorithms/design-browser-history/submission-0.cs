b1ae3b276f599a63ebd3501c8705d70be17c6717
HoangMinhOG <vuhoangminh30092001@gmail.com>
2026-08-13 11:31:20 -0400

    Add: design-browser-history - submission-0

---
+++ Data Structures & Algorithms/design-browser-history/submission-0.cs
@@ -0,0 +1,44 @@
+public class BrowserHistory {
+    int currentIndex;
+    int maxIndex;
+    List<string> history;
+
+    public BrowserHistory(string homepage) {
+        history = new List<string>();
+        history.Add(homepage);
+
+        currentIndex=0;
+        maxIndex=0;
+        
+    }
+    
+    public void Visit(string url) {
+        currentIndex++;
+        if(currentIndex<history.Count){
+            history[currentIndex]=url;
+        }
+        else{
+            history.Add(url);
+        }
+        maxIndex = currentIndex;
+    }
+    
+    public string Back(int steps) {
+        currentIndex = Math.Max(0, currentIndex-steps);
+        return history[currentIndex];
+        
+    }
+    
+    public string Forward(int steps) {
+        currentIndex = Math.Min(maxIndex,currentIndex+steps);
+        return history[currentIndex];
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.Visit(url);
+ * string param_2 = obj.Back(steps);
+ * string param_3 = obj.Forward(steps);
+ */
