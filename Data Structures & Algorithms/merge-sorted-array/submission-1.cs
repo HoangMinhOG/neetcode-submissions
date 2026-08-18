e181e24ba0c18588bf96b8ec23f452e3b40176b5
HoangMinhOG <vuhoangminh30092001@gmail.com>
2026-08-18 15:47:41 -0400

    Add: merge-sorted-array - submission-1

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-1.cs
@@ -0,0 +1,32 @@
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int i = m;
+        int j = n;
+        int k = m + n - 1;
+
+        while (i > 0 && j > 0)
+        {
+            if (nums1[i - 1] < nums2[j - 1])
+            {
+                nums1[k] = nums2[j - 1];
+                j--;
+            }
+            else
+            {
+                nums1[k] = nums1[i - 1];
+                i--;
+            }
+
+            k--;
+        }
+
+        while (j > 0)
+        {
+            nums1[k] = nums2[j - 1];
+            j--;
+            k--;
+        }
+    }
+}
