464eb6a816e74149c89dc11d8712914f3df23f9a
HoangMinhOG <vuhoangminh30092001@gmail.com>
2026-09-14 14:54:02 -0400

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cs
@@ -0,0 +1,24 @@
+public class KthLargest {
+    PriorityQueue<int, int> queue = new PriorityQueue<int,int>();
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k=k;
+        for(int i=0;i<nums.Length;i++){
+            queue.Enqueue(nums[i],nums[i]);
+        }
+        while(queue.Count>k){
+            queue.Dequeue();
+        }
+        
+    }
+    
+    public int Add(int val) {
+        queue.Enqueue(val,val);
+        while(queue.Count>k){
+            queue.Dequeue();
+        }
+        return queue.Peek();
+    }
+   
+}
